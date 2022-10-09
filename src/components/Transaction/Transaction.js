c08af511338e1ecff4d4945a9a064787c7975f98
Haklifesavr <hak173129@gmail.com>
2022-10-09 16:26:37 +0500

    added transactions

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -13,6 +13,20 @@ function Transaction() {
   const[txid, setTxid] = useState(false);
   const web3ModalRef = useRef();
 
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   const connectWallet = async () => {
     try {
       // Get the provider from web3Modal, which in our case is MetaMask
@@ -77,20 +91,6 @@ function Transaction() {
     }
     return web3Provider;
   };
-
-  useEffect(() => {
-    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
-    if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
-      // The `current` value is persisted throughout as long as this page is open
-      web3ModalRef.current = new Web3Modal({
-        network: "goerli",
-        providerOptions: {},
-        disableInjectedProvider: false,
-      });
-      connectWallet();
-    }
-  }, [walletConnected]);
 
   return (
     <Box
