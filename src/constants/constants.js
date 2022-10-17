590127f749c9744f5354e04082e5b363d2f778b3
IamTalha_Sajid <talha.sajid@fiveriverstech.com>
2022-10-17 12:15:23 +0500

    Updated Smart Contract and Removed Extra Folder

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,240 +1,270 @@
-export const TOKEN_ADDRESS = "0x61a1d7DBBB5c54bb563C6Edc8C01b3CA9fa0a974";
+export const TOKEN_ADDRESS = "0x4Df48189eA65ed857B7885F1E081f4A78f3Ef98F";
 export const abi = [
   {
-    "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
   },
   {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "productID",
-        "type": "uint256"
-      }
-    ],
-    "name": "approveRequest",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "checkTransactionStatus",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "txId",
-        "type": "uint256"
-      }
-    ],
-    "name": "getTransactionDetails",
-    "outputs": [
-      {
-        "components": [
+      "inputs": [
           {
-            "internalType": "uint256",
-            "name": "txId",
-            "type": "uint256"
-          },
-          {
-            "internalType": "address",
-            "name": "from",
-            "type": "address"
-          },
-          {
-            "internalType": "address",
-            "name": "to",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "productId",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "approved",
-            "type": "bool"
+              "internalType": "uint256",
+              "name": "productID",
+              "type": "uint256"
           }
-        ],
-        "internalType": "struct amazonTransparancy.transaction",
-        "name": "",
-        "type": "tuple"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
+      ],
+      "name": "approveRequest",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
   },
   {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "productId",
-        "type": "uint256"
-      },
-      {
-        "internalType": "address",
-        "name": "_customerAddress",
-        "type": "address"
-      }
-    ],
-    "name": "markAsDelivered",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+      "inputs": [],
+      "name": "checkTransactionStatus",
+      "outputs": [
+          {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
   },
   {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "productID",
-        "type": "uint256"
-      }
-    ],
-    "name": "performTransaction",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
+      "inputs": [
+          {
+              "internalType": "uint256",
+              "name": "txId",
+              "type": "uint256"
+          }
+      ],
+      "name": "getTransactionDetails",
+      "outputs": [
+          {
+              "components": [
+                  {
+                      "internalType": "uint256",
+                      "name": "txId",
+                      "type": "uint256"
+                  },
+                  {
+                      "internalType": "address",
+                      "name": "from",
+                      "type": "address"
+                  },
+                  {
+                      "internalType": "address",
+                      "name": "to",
+                      "type": "address"
+                  },
+                  {
+                      "internalType": "uint256",
+                      "name": "productId",
+                      "type": "uint256"
+                  },
+                  {
+                      "internalType": "bool",
+                      "name": "approved",
+                      "type": "bool"
+                  }
+              ],
+              "internalType": "struct amazonTransparency.transaction",
+              "name": "",
+              "type": "tuple"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
   },
   {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "supplierAddress",
-        "type": "address"
-      },
-      {
-        "internalType": "address",
-        "name": "retailerAddress",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "productID",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "retailerRequiredUnits",
-        "type": "uint256"
-      }
-    ],
-    "name": "productRequesting",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+      "inputs": [
+          {
+              "internalType": "uint256",
+              "name": "productId",
+              "type": "uint256"
+          },
+          {
+              "internalType": "address",
+              "name": "_customerAddress",
+              "type": "address"
+          }
+      ],
+      "name": "markAsDelivered",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
   },
   {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "productRequests",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "requestId",
-        "type": "uint256"
-      },
-      {
-        "internalType": "address",
-        "name": "retailerAddress",
-        "type": "address"
-      },
-      {
-        "internalType": "address",
-        "name": "supplierAddress",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "productID",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "retailerRequiredUnits",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "paymentTransaction",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
+      "inputs": [
+          {
+              "internalType": "uint256",
+              "name": "productID",
+              "type": "uint256"
+          }
+      ],
+      "name": "performTransaction",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "payable",
+      "type": "function"
   },
   {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "products",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "productID",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "isDeliveredToCustomer",
-        "type": "bool"
-      },
-      {
-        "internalType": "address",
-        "name": "productOwner",
-        "type": "address"
-      },
-      {
-        "internalType": "bool",
-        "name": "paymentApproved",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
+      "inputs": [
+          {
+              "internalType": "address",
+              "name": "supplierAddress",
+              "type": "address"
+          },
+          {
+              "internalType": "uint256",
+              "name": "productID",
+              "type": "uint256"
+          },
+          {
+              "internalType": "uint256",
+              "name": "retailerRequiredUnits",
+              "type": "uint256"
+          }
+      ],
+      "name": "productRequesting",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
   },
   {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "retailerStoreName",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "supplierName",
-        "type": "string"
-      },
-      {
-        "internalType": "address",
-        "name": "supplierAddress",
-        "type": "address"
-      }
-    ],
-    "name": "registeration",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+      "inputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "name": "productRequests",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "requestId",
+              "type": "uint256"
+          },
+          {
+              "internalType": "address",
+              "name": "retailerAddress",
+              "type": "address"
+          },
+          {
+              "internalType": "address",
+              "name": "supplierAddress",
+              "type": "address"
+          },
+          {
+              "internalType": "uint256",
+              "name": "productID",
+              "type": "uint256"
+          },
+          {
+              "internalType": "uint256",
+              "name": "retailerRequiredUnits",
+              "type": "uint256"
+          },
+          {
+              "internalType": "bool",
+              "name": "paymentTransaction",
+              "type": "bool"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "name": "products",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "productID",
+              "type": "uint256"
+          },
+          {
+              "internalType": "bool",
+              "name": "isDeliveredToCustomer",
+              "type": "bool"
+          },
+          {
+              "internalType": "address",
+              "name": "productOwner",
+              "type": "address"
+          },
+          {
+              "internalType": "bool",
+              "name": "paymentApprovedByAmazon",
+              "type": "bool"
+          },
+          {
+              "internalType": "bool",
+              "name": "isExist",
+              "type": "bool"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "string",
+              "name": "retailerStoreName",
+              "type": "string"
+          },
+          {
+              "internalType": "string",
+              "name": "supplierName",
+              "type": "string"
+          },
+          {
+              "internalType": "address",
+              "name": "supplierAddress",
+              "type": "address"
+          }
+      ],
+      "name": "registeration",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
   }
 ]
